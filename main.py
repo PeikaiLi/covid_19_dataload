ff8ab0558c3a1875a5622fdeded79b40a8be5d95
Peikai_li <Peikai_li@163.com>
2022-09-13 20:46:14 +0800

    修复bug

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ class Listener:
         dirs =  os.path.join(os.path.split(os.path.realpath(__file__))[0], 'json')
         if not os.path.exists(dirs):
             os.makedirs(dirs)
-        with open(os.path.join(dirs, 'collection' + '.json'),'w', encoding='utf-8') as json_file:
+        with open(os.path.join(dirs, collection + '.json'),'w', encoding='utf-8') as json_file:
             json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 
